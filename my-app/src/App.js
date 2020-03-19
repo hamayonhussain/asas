7d4296e694ab23d1387086a31e0d6db24796aa75
Hamayon Hussain <hamayon.hussain@gmail.com>
2020-03-19 15:49:31 -0500

    Image src changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Nav, Jumbotron, Container, Image } from 'react-bootstrap';
+import { Navbar, Nav, Jumbotron, Container, Media } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
@@ -23,17 +23,21 @@ class App extends React.Component {
         <div className="black-bg">
           <Jumbotron fluid>
             <Container>
-              <h1 >American Safeguard Alliance LLC</h1>
+              <Media>
+              <img
+                  width={64}
+                  height={64}
+                  className="align-self-end mr-3"
+                  src={process.env.PUBLIC_URL + "public/asas-patch.png"}
+                  alt="Generic placeholder"
+                />
+              </Media>
+              <h1 >American Safeguard Alliance</h1>
               <p>
                 Security Guard Company
               </p>
             </Container>
           </Jumbotron>
-        </div>
-
-        {/* asas image */}
-        <div>
-        <Image src="public/asas-patch.png" fluid />
         </div>
       </Fragment>
     )
