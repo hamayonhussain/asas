783dea20a762c61ba083c8fd139c97526b7c6503
Hamayon Hussain <hamayon.hussain@gmail.com>
2020-03-30 03:04:36 -0500

    added a few more containers and texts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,8 +13,9 @@ class App extends React.Component {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#services">Services</Nav.Link>
               <Nav.Link href="#careers">Careers</Nav.Link>
-              <Nav.Link href="#contactus">Contact Us</Nav.Link>
+              <Nav.Link href="#contactus">Contact us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
@@ -32,13 +33,41 @@ class App extends React.Component {
                   alt="Generic placeholder"
                 />
               </Media>
-              <h1 >American Safeguard Alliance</h1>
+              <h2>American Safeguard Alliance</h2>
               <p>
                 Security Guard Company
+              </p>
+              <p>
+                License # B11605101
               </p>
             </Container>
           </Jumbotron>
         </div>
+
+        <div>
+          <Container>
+          <h1 id="about" className="text-center">About</h1>
+            <p className="text-center">
+              American Safeguard Alliance offers security guard services in Houston, Dallas & Austin area. Our company is registered with the state of texas
+              and our security officers get trained by a security academy getting certified in Level 2, 3 or 4. We cover a wide array of security work
+            </p>
+          </Container>
+        </div>
+
+        <Container>
+        <h1 className="text-center" id="services">Services</h1>
+          <ul>
+            <li>Unarmed</li>
+            <li>Armed</li>
+            <li>Personal Protection Officers</li>
+            <li>Property Patrol Inspections</li>
+            <li>Construction Sites</li>
+            <li>Corporate</li>
+            <li>Shopping Center</li>
+            <li>Residential Subdivision</li>
+            <li>Firewatch</li>
+          </ul>
+        </Container>
       </Fragment>
     )
   }
