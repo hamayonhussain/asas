e83111acf0f078f18fb4847dfab05ff864884680
hamayonhussain <hamayon.hussain@gmail.com>
2020-08-14 22:47:14 -0500

    created multiple components and moved code accordingly

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -4,10 +4,28 @@ import { DropdownDivider } from 'react-bootstrap/Dropdown';
 
 export default function Footer() {
     return (
-        <div className="main-footer">
-          <div className="container">
-            </div>  
-
-        </div>
+        <footer classNameName="footer bg-light">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
+                    <p className="text-muted small mb-4 mb-lg-0">© HT POOL SOLUTIONS 2020 All Rights Reserved.</p>
+                    </div>
+                    <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
+                    <ul className="list-inline mb-0">
+                        <li className="list-inline-item mr-3">
+                        <a href="https://www.facebook.com/407548936768256/posts/407574273432389?sfns=mo">
+                            <i class="fab fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                        </li>
+                        <li className="list-inline-item">
+                        <a href="https://www.instagram.com/htpoolsolutions/">
+                            <i className="fab fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,105 @@
+     
+     
+     {/* Header */}
+     <Container>
+
+     <header>
+     <h3>
+       Trusted and Reliable Security Solutions
+     </h3>
+     <p>We offer professional and reliable security service to multiple industries in Houston and the surrounding area. 
+     Through consultation with our clients, we provide a custom solution that fulfills their needs as well as maximizes the standard of security. 
+     Choose the kind of security service that resolve your unique security concerns as well as lie in your budget. Get in touch to get a proposal today!
+     </p>
+     </header>
+
+     </Container>
+
+     
+       <Container>
+
+       <h3>
+         Among the Leading Security Companies in Houston
+       </h3>
+
+         <p>
+         Our experience enables us to provide a complete security package that suits each client's individual needs. 
+         We offer a range of services, which means you need not look any further for your security requirements.
+         </p>
+
+         <p>
+         American Safeguard Alliance LLC is an acclaimed firm of security agents based in Houston, Texas. Our clients trust us for our effective security 
+         solutions and the use of innovative technology to protect life and property. We provide certified, licensed, insured, and bonded Security Guards 
+         who are screened to the standards of Texas Private Security Bureau. It ensures they are well trained and possess adequate 
+         experience in their field to solve any security problem swiftly. 
+         </p>
+
+         <p>
+         We take pride in our professionally dressed security agents who follow a strict code of conduct while effectively securing your valuable assets.
+         </p>
+
+       </Container>
+     
+
+     <Container>
+       <h3>
+         Our Services
+       </h3>
+
+       <p >American Safeguard Alliance is committed to delivering Total Quality Management by providing services that ensure quality 
+       and safety while meeting client's expectations.
+       </p>
+
+       <ul>
+         <li>Unarmed Security Service</li>
+         <li>Armed Security Service</li>
+         <li>Personal Protection Officers</li>
+         <li>Property Patrol Inspections</li>
+         <li>Construction Sites Security Service</li>
+         <li>Corporate Security Service</li>
+         <li>Shopping Center Security Service</li>
+         <li>Residential Subdivision Security Service</li>
+         <li>Firewatch Security Service</li>
+       </ul>
+     </Container>
+
+     <Container>
+       <h3>
+         Why Choose Us?
+       </h3>
+
+       <p>
+         American Safeguard Alliance is a trusted security partner in Houston and surrounding areas. We possess adequate experience and expertise and show strict 
+         professionalism in our conduct to give you total peace of mind.
+       </p>
+
+       <p>
+         <strong>Highly trained and experienced security guards: </strong>
+         We select security guards based on character and experience and then refine their expertise with continuing education and site-specific training. 
+         It ensures that they have specific training, qualification, and expertise to render distinguished <strong>Courteous and discreet service: </strong>
+         With our continued training, we provide a positive work environment for our officers. It contributes to their loyalty,
+          along with an industry-low rate of attrition. Thus, we can place a high emphasis on customer service, professionalism, and discretion.
+       </p>
+
+       <p>
+         <strong>Custom security solutions: </strong>We work with you as your security partner to custom solutions that match your unique needs and fit in your security budget.
+         We deploy state of the art technology to deliver unmatched security for your assets, property, and personnel. 
+       </p>
+
+       <p>
+         <strong>24/7 Surveillance: </strong>Whether you are looking for an overtime security agency for your commercial building or the one that is always available, with our 24/7/365 surveillance,
+         we keep you safe around the clock. Your security is our priority; that's why you can always trust us for your security needs.
+       </p>
+
+       <p>
+         <strong>Effective security technology: </strong>We implement the best security options to protect your valuable assets. Whether it is the use of a simple wireless communication system or GPS tracking of security guards,
+         we always use the best and latest options to provide you with adequate security solutions. We keep a tab on the latest technological innovation in the security landscape and make them available as early as possible.
+       </p>
+
+       <p>
+         The advancement of technology has become the reason for the improvement in the security scenario, but there are still situations where it can be compromised. Therefore, 
+         the need for an exclusive agency that can monitor and undertake all the required security services is inevitable. We let you focus on your work without worrying about security by protecting your resources and assets. 
+         Get in touch to know how we can resolve your security concerns.
+       </p>
+     </Container>
+   
